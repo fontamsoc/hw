651b6ba92c246647c89e21edc5e11e7ed9fcac02
William Tambe <tambewilliam@gmail.com>
2022-03-11 13:57:57 -0600

    pu32-sim: dcacheinit improvements

--- pu32-sim/dcacheinit/dcacheinit.c
+++ pu32-sim/dcacheinit/dcacheinit.c
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // (c) William Fonkou Tambe
 
-#include <stdint.h>
-
 // Used to stringify.
 #define __xstr__(s) __str__(s)
 #define __str__(s) #s
 
-#define STACKSZ 16 /* computed from -fstack-usage outputs */
+#define __STACKSZ 16 /* computed from -fstack-usage outputs */
 
-static unsigned long stack[STACKSZ] __attribute__((used));
+static unsigned long __stack[__STACKSZ] __attribute__((used));
 
 // Substitute for crt0.S since this is built using -nostdlib.
 __asm__ (
@@ -20,27 +18,38 @@ __asm__ (
 	"_start:\n"
 
 	// Initialize %sp and %fp.
-	"rli16 %sp, stack + "__xstr__(STACKSZ)"\n" // ### Manually encoding rli16 since linker-relaxation is not yet implemented.
+	"rli16 %sp, __stack + "__xstr__(__STACKSZ)"\n"
 	//"li8 %fp, 0\n" // ### Disabled, as it is unnecessary.
-	"rli16 %sr, main\n" // ### Manually encoding rli16 since linker-relaxation is not yet implemented.
-	"j %sr\n" // ### Note that %rp is expected to have been properly set.
+	"rli16 %sr, main\n"
+	"j %sr\n" // ### %rp is expected to have been properly set.
 
 	".size    _start, (. - _start)\n");
 
-#define UARTADDR (0x0ff8 /* By convention, the first UART is located at 0x0ff8 */)
-#define UARTBAUD 115200
+#define __SERIAL0_ADDR (0x0ff8 /* By convention, the first UART is located at 0x0ff8 */)
+#define __SERIAL0_BAUD 115200
 
 #include <hwdrvchar/hwdrvchar.h>
-hwdrvchar hwdrvchar_dev = {.addr = (void *)UARTADDR};
+static hwdrvchar __hwdrvchar_dev = {.addr = (void *)__SERIAL0_ADDR};
+static inline void serial0_init (void) {
+	hwdrvchar_init (&__hwdrvchar_dev, __SERIAL0_BAUD);
+}
 
-int putchar (int c) {
-	while (!hwdrvchar_write(&hwdrvchar_dev, &c, 1));
+static int putchar (int c) {
+	while (!hwdrvchar_write(&__hwdrvchar_dev, &c, 1));
 	return c;
 }
 
-#include <stdio.h>
+static int puts (char *s) {
+	char *_s = s;
+	unsigned char c;
+	while (c = *_s) {
+		putchar(c);
+		++_s;
+	}
+	return ((int)_s - (int)s);
+}
 
 void main (void) {
-	hwdrvchar_init (&hwdrvchar_dev, UARTBAUD);
+	serial0_init();
 	puts("ram initialized\n");
 }
