ab2df531d5a7c5e575c5a356375b1a56b48d64cc
William Tambe <tambewilliam@gmail.com>
2022-03-11 13:59:46 -0600

    pu32-nexys4ddr: LiteDRAM (2d021c8) Migen (7014bdc) LiteX (75134605)

--- pu32-nexys4ddr/litedram/litedram.h
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-// (c) William Fonkou Tambe
-
-#define STACKSZ  256 /* computed from -fstack-usage outputs */
-#define CSR_BASE (0x8000000+0x1000) /* control interface address */
