7fccaf8973f2f45a1415ff1faa02ae377223638c
William Tambe <tambewilliam@gmail.com>
2022-03-18 09:27:51 -0500

    dev/devtbl.v: use RAMCACHESZ default of 2

--- dev/devtbl.v
+++ dev/devtbl.v
@@ -30,7 +30,7 @@ module devtbl (
 `include "lib/clog2.v"
 
 parameter ARCHBITSZ   = 16;
-parameter RAMCACHESZ  = 0; // Size of the RAM cache in (ARCHBITSZ/8) bytes.
+parameter RAMCACHESZ  = 2; // Size of the RAM cache in (ARCHBITSZ/8) bytes.
 parameter PRELDRADDR  = 0; // Address of pre-loader.
 parameter DEVMAPCNT   = 2; // Number of device mappings; must be <= (((4096-512)/(ARCHBITSZ/8))-1).
 
