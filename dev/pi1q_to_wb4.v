edc77b0e6e72fb59e72470c1155f795b683237f6
William Tambe <tambewilliam@gmail.com>
2022-08-24 10:14:12 -0500

    pi1q_to_wb4: support multiple masters

--- dev/pi1q_to_wb4.v
+++ dev/pi1q_to_wb4.v
@@ -34,6 +34,8 @@ module pi1q_to_wb4 (
 
 `include "lib/clog2.v"
 
+parameter MASTERCOUNT = 1;
+
 parameter ARCHBITSZ = 16;
 
 localparam CLOG2ARCHBITSZBY8 = clog2(ARCHBITSZ/8);
@@ -42,13 +44,13 @@ localparam ADDRBITSZ = (ARCHBITSZ-CLOG2ARCHBITSZBY8);
 
 input wire wb4_rst_i;
 
-input wire                         pi1_clk_i;
-input  wire [2 -1 : 0]             pi1_op_i;
-input  wire [ADDRBITSZ -1 : 0]     pi1_addr_i;
-input  wire [ARCHBITSZ -1 : 0]     pi1_data_i;
-output wire [ARCHBITSZ -1 : 0]     pi1_data_o;
-input  wire [(ARCHBITSZ/8) -1 : 0] pi1_sel_i;
-output wire                        pi1_rdy_o;
+input  wire                                        pi1_clk_i;
+input  wire [(2 * MASTERCOUNT) -1 : 0]             pi1_op_i;
+input  wire [(ADDRBITSZ * MASTERCOUNT) -1 : 0]     pi1_addr_i;
+input  wire [(ARCHBITSZ * MASTERCOUNT) -1 : 0]     pi1_data_i;
+output wire [(ARCHBITSZ * MASTERCOUNT) -1 : 0]     pi1_data_o;
+input  wire [((ARCHBITSZ/8) * MASTERCOUNT) -1 : 0] pi1_sel_i;
+output wire [(1 * MASTERCOUNT) -1 : 0]             pi1_rdy_o;
 
 input wire                        wb4_clk_i;
 output reg                        wb4_cyc_o;
@@ -61,7 +63,7 @@ input wire                        wb4_stall_i;
 input wire                        wb4_ack_i;
 input wire [ARCHBITSZ -1 : 0]     wb4_data_i;
 
-localparam PI1QMASTERCOUNT       = 1;
+localparam PI1QMASTERCOUNT       = MASTERCOUNT;
 localparam PI1QARCHBITSZ         = ARCHBITSZ;
 localparam CLOG2PI1QARCHBITSZBY8 = clog2(PI1QARCHBITSZ/8);
 localparam PI1QADDRBITSZ         = (PI1QARCHBITSZ-CLOG2PI1QARCHBITSZBY8);
@@ -86,12 +88,15 @@ wire s_pi1q_clk_w = wb4_clk_i;
 // 	input                             s_pi1q_rdy_w;
 `include "lib/perint/inst.pi1q.v"
 
-assign m_pi1q_op_w[0] = pi1_op_i;
-assign m_pi1q_addr_w[0] = pi1_addr_i;
-assign m_pi1q_data_w1[0] = pi1_data_i;
-assign pi1_data_o = m_pi1q_data_w0[0];
-assign m_pi1q_sel_w[0] = pi1_sel_i;
-assign pi1_rdy_o = m_pi1q_rdy_w[0];
+genvar gen_m_pi1q_idx;
+generate for (gen_m_pi1q_idx = 0; gen_m_pi1q_idx < MASTERCOUNT; gen_m_pi1q_idx = gen_m_pi1q_idx + 1) begin :gen_m_pi1q
+assign m_pi1q_op_w[gen_m_pi1q_idx] = pi1_op_i[((gen_m_pi1q_idx+1)*2)-1:gen_m_pi1q_idx*2];
+assign m_pi1q_addr_w[gen_m_pi1q_idx] = pi1_addr_i[((gen_m_pi1q_idx+1)*ADDRBITSZ)-1:gen_m_pi1q_idx*ADDRBITSZ];
+assign m_pi1q_data_w1[gen_m_pi1q_idx] = pi1_data_i[((gen_m_pi1q_idx+1)*ARCHBITSZ)-1:gen_m_pi1q_idx*ARCHBITSZ];
+assign pi1_data_o[((gen_m_pi1q_idx+1)*ARCHBITSZ)-1:gen_m_pi1q_idx*ARCHBITSZ] = m_pi1q_data_w0[gen_m_pi1q_idx];
+assign m_pi1q_sel_w[gen_m_pi1q_idx] = pi1_sel_i[((gen_m_pi1q_idx+1)*(ARCHBITSZ/8))-1:gen_m_pi1q_idx*(ARCHBITSZ/8)];
+assign pi1_rdy_o[((gen_m_pi1q_idx+1)*1)-1:gen_m_pi1q_idx*1] = m_pi1q_rdy_w[gen_m_pi1q_idx];
+end endgenerate
 
 localparam PINOOP = 2'b00;
 localparam PIWROP = 2'b01;
