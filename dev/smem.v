1524d7ba3d77a6346fb5c138f6f899eea70b3824
William Tambe <tambewilliam@gmail.com>
2023-04-13 12:35:22 -0500

    dev/smem: better logic to generate sel_w

--- dev/smem.v
+++ dev/smem.v
@@ -91,37 +91,36 @@ reg [CNTRBITSZ -1 : 0] cntr = 0;
 
 assign pi1_rdy_o = !cntr;
 
-wire [(256/*ARCHBITSZ*//8) -1 : 0] _pi1_sel_i = pi1_sel_i;
-// ### Net declared as reg so as to be useable by verilog within the always block.
-reg [ARCHBITSZ -1 : 0] sel_w; // Net set to a bitmask used to modify only a portion of the indexed memory.
-always @* begin
-	if (ARCHBITSZ == 16)
-		sel_w = {{8{_pi1_sel_i[1]}}, {8{_pi1_sel_i[0]}}};
-	else if (ARCHBITSZ == 32)
-		sel_w = {{8{_pi1_sel_i[3]}}, {8{_pi1_sel_i[2]}}, {8{_pi1_sel_i[1]}}, {8{_pi1_sel_i[0]}}};
-	else if (ARCHBITSZ == 64)
-		sel_w = {
-			{8{_pi1_sel_i[7]}}, {8{_pi1_sel_i[6]}}, {8{_pi1_sel_i[5]}}, {8{_pi1_sel_i[4]}},
-			{8{_pi1_sel_i[3]}}, {8{_pi1_sel_i[2]}}, {8{_pi1_sel_i[1]}}, {8{_pi1_sel_i[0]}}};
-	else if (ARCHBITSZ == 128)
-		sel_w = {
-			{8{_pi1_sel_i[15]}}, {8{_pi1_sel_i[14]}}, {8{_pi1_sel_i[13]}}, {8{_pi1_sel_i[12]}},
-			{8{_pi1_sel_i[11]}}, {8{_pi1_sel_i[10]}}, {8{_pi1_sel_i[9]}}, {8{_pi1_sel_i[8]}},
-			{8{_pi1_sel_i[7]}}, {8{_pi1_sel_i[6]}}, {8{_pi1_sel_i[5]}}, {8{_pi1_sel_i[4]}},
-			{8{_pi1_sel_i[3]}}, {8{_pi1_sel_i[2]}}, {8{_pi1_sel_i[1]}}, {8{_pi1_sel_i[0]}}};
-	else if (ARCHBITSZ == 256)
-		sel_w = {
-			{8{_pi1_sel_i[31]}}, {8{_pi1_sel_i[30]}}, {8{_pi1_sel_i[29]}}, {8{_pi1_sel_i[28]}},
-			{8{_pi1_sel_i[27]}}, {8{_pi1_sel_i[26]}}, {8{_pi1_sel_i[25]}}, {8{_pi1_sel_i[24]}},
-			{8{_pi1_sel_i[23]}}, {8{_pi1_sel_i[22]}}, {8{_pi1_sel_i[21]}}, {8{_pi1_sel_i[20]}},
-			{8{_pi1_sel_i[19]}}, {8{_pi1_sel_i[18]}}, {8{_pi1_sel_i[17]}}, {8{_pi1_sel_i[16]}},
-			{8{_pi1_sel_i[15]}}, {8{_pi1_sel_i[14]}}, {8{_pi1_sel_i[13]}}, {8{_pi1_sel_i[12]}},
-			{8{_pi1_sel_i[11]}}, {8{_pi1_sel_i[10]}}, {8{_pi1_sel_i[9]}}, {8{_pi1_sel_i[8]}},
-			{8{_pi1_sel_i[7]}}, {8{_pi1_sel_i[6]}}, {8{_pi1_sel_i[5]}}, {8{_pi1_sel_i[4]}},
-			{8{_pi1_sel_i[3]}}, {8{_pi1_sel_i[2]}}, {8{_pi1_sel_i[1]}}, {8{_pi1_sel_i[0]}}};
-	else
-		sel_w = {ARCHBITSZ{1'b0}};
-end
+wire [ARCHBITSZ -1 : 0] sel_w; // Net set to a bitmask used to modify only a portion of the indexed memory.
+generate if (ARCHBITSZ == 16) begin
+	assign sel_w = {{8{pi1_sel_i[1]}}, {8{pi1_sel_i[0]}}};
+end endgenerate
+generate if (ARCHBITSZ == 32) begin
+	assign sel_w = {{8{pi1_sel_i[3]}}, {8{pi1_sel_i[2]}}, {8{pi1_sel_i[1]}}, {8{pi1_sel_i[0]}}};
+end endgenerate
+generate if (ARCHBITSZ == 64) begin
+	assign sel_w = {
+		{8{pi1_sel_i[7]}}, {8{pi1_sel_i[6]}}, {8{pi1_sel_i[5]}}, {8{pi1_sel_i[4]}},
+		{8{pi1_sel_i[3]}}, {8{pi1_sel_i[2]}}, {8{pi1_sel_i[1]}}, {8{pi1_sel_i[0]}}};
+end endgenerate
+generate if (ARCHBITSZ == 128) begin
+	assign sel_w = {
+		{8{pi1_sel_i[15]}}, {8{pi1_sel_i[14]}}, {8{pi1_sel_i[13]}}, {8{pi1_sel_i[12]}},
+		{8{pi1_sel_i[11]}}, {8{pi1_sel_i[10]}}, {8{pi1_sel_i[9]}}, {8{pi1_sel_i[8]}},
+		{8{pi1_sel_i[7]}}, {8{pi1_sel_i[6]}}, {8{pi1_sel_i[5]}}, {8{pi1_sel_i[4]}},
+		{8{pi1_sel_i[3]}}, {8{pi1_sel_i[2]}}, {8{pi1_sel_i[1]}}, {8{pi1_sel_i[0]}}};
+end endgenerate
+generate if (ARCHBITSZ == 256) begin
+	assign sel_w = {
+		{8{pi1_sel_i[31]}}, {8{pi1_sel_i[30]}}, {8{pi1_sel_i[29]}}, {8{pi1_sel_i[28]}},
+		{8{pi1_sel_i[27]}}, {8{pi1_sel_i[26]}}, {8{pi1_sel_i[25]}}, {8{pi1_sel_i[24]}},
+		{8{pi1_sel_i[23]}}, {8{pi1_sel_i[22]}}, {8{pi1_sel_i[21]}}, {8{pi1_sel_i[20]}},
+		{8{pi1_sel_i[19]}}, {8{pi1_sel_i[18]}}, {8{pi1_sel_i[17]}}, {8{pi1_sel_i[16]}},
+		{8{pi1_sel_i[15]}}, {8{pi1_sel_i[14]}}, {8{pi1_sel_i[13]}}, {8{pi1_sel_i[12]}},
+		{8{pi1_sel_i[11]}}, {8{pi1_sel_i[10]}}, {8{pi1_sel_i[9]}}, {8{pi1_sel_i[8]}},
+		{8{pi1_sel_i[7]}}, {8{pi1_sel_i[6]}}, {8{pi1_sel_i[5]}}, {8{pi1_sel_i[4]}},
+		{8{pi1_sel_i[3]}}, {8{pi1_sel_i[2]}}, {8{pi1_sel_i[1]}}, {8{pi1_sel_i[0]}}};
+end endgenerate
 
 wire en_w = (pi1_rdy_o && (pi1_op_i == PIRDOP || pi1_op_i == PIRWOP));
 wire we_w = (pi1_rdy_o && (pi1_op_i == PIWROP || pi1_op_i == PIRWOP));
