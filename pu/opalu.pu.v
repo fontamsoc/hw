df40c7737035577bb9888cf52e6bba963d5717bf
William Tambe <tambewilliam@gmail.com>
2023-05-01 18:54:23 -0500

    pu: use switch case in opalu and opgetsysreg

--- pu/opalu.pu.v
+++ pu/opalu.pu.v
@@ -3,82 +3,98 @@
 
 always @* begin
 	// Implement sgt, sgte, sgtu, sgteu.
-	if      (isoptype0) opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) > $signed(gprdata2)};
-	else if (isoptype1) opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) >= $signed(gprdata2)};
-	else if (isoptype2) opalu0result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 > gprdata2};
-	else                opalu0result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 >= gprdata2};
+	case (instrbufdato0[2:0])
+	0:       opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) > $signed(gprdata2)};
+	1:       opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) >= $signed(gprdata2)};
+	2:       opalu0result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 > gprdata2};
+	default: opalu0result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 >= gprdata2};
+	endcase
 end
 `ifdef PUSC2
 always @* begin
-	if      (sc2isoptype0) sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) > $signed(sc2gprdata2)};
-	else if (sc2isoptype1) sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) >= $signed(sc2gprdata2)};
-	else if (sc2isoptype2) sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 > sc2gprdata2};
-	else                   sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 >= sc2gprdata2};
+	case (sc2instrbufdato0[2:0])
+	0:       sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) > $signed(sc2gprdata2)};
+	1:       sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) >= $signed(sc2gprdata2)};
+	2:       sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 > sc2gprdata2};
+	default: sc2opalu0result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 >= sc2gprdata2};
+	endcase
 end
 `endif
 
 always @* begin
 	// Implement add, sub, seq, sne, slt, slte, sltu, slteu.
-	if      (isoptype0) opalu1result = gprdata1 + gprdata2;
-	else if (isoptype1) opalu1result = gprdata1 - gprdata2;
-	else if (isoptype2) opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 == gprdata2};
-	else if (isoptype3) opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 != gprdata2};
-	else if (isoptype4) opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) < $signed(gprdata2)};
-	else if (isoptype5) opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) <= $signed(gprdata2)};
-	else if (isoptype6) opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 < gprdata2};
-	else                opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 <= gprdata2};
+	case (instrbufdato0[2:0])
+	0:       opalu1result = gprdata1 + gprdata2;
+	1:       opalu1result = gprdata1 - gprdata2;
+	2:       opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 == gprdata2};
+	3:       opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 != gprdata2};
+	4:       opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) < $signed(gprdata2)};
+	5:       opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(gprdata1) <= $signed(gprdata2)};
+	6:       opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 < gprdata2};
+	default: opalu1result = {{(ARCHBITSZ-1){1'b0}}, gprdata1 <= gprdata2};
+	endcase
 end
 `ifdef PUSC2
 always @* begin
-	if      (sc2isoptype0) sc2opalu1result = sc2gprdata1 + sc2gprdata2;
-	else if (sc2isoptype1) sc2opalu1result = sc2gprdata1 - sc2gprdata2;
-	else if (sc2isoptype2) sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 == sc2gprdata2};
-	else if (sc2isoptype3) sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 != sc2gprdata2};
-	else if (sc2isoptype4) sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) < $signed(sc2gprdata2)};
-	else if (sc2isoptype5) sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) <= $signed(sc2gprdata2)};
-	else if (sc2isoptype6) sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 < sc2gprdata2};
-	else                   sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 <= sc2gprdata2};
+	case (sc2instrbufdato0[2:0])
+	0:       sc2opalu1result = sc2gprdata1 + sc2gprdata2;
+	1:       sc2opalu1result = sc2gprdata1 - sc2gprdata2;
+	2:       sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 == sc2gprdata2};
+	3:       sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 != sc2gprdata2};
+	4:       sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) < $signed(sc2gprdata2)};
+	5:       sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, $signed(sc2gprdata1) <= $signed(sc2gprdata2)};
+	6:       sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 < sc2gprdata2};
+	default: sc2opalu1result = {{(ARCHBITSZ-1){1'b0}}, sc2gprdata1 <= sc2gprdata2};
+	endcase
 end
 `endif
 
 always @* begin
 	// Implement sll, srl, sra, and, or, xor, not, cpy.
-	if      (isoptype0) opalu2result = gprdata1 << gprdata2[CLOG2ARCHBITSZ-1:0];
-	else if (isoptype1) opalu2result = gprdata1 >> gprdata2[CLOG2ARCHBITSZ-1:0];
-	else if (isoptype2) opalu2result = $signed(gprdata1) >>> gprdata2[CLOG2ARCHBITSZ-1:0];
-	else if (isoptype3) opalu2result = gprdata1 & gprdata2;
-	else if (isoptype4) opalu2result = gprdata1 | gprdata2;
-	else if (isoptype5) opalu2result = gprdata1 ^ gprdata2;
-	else if (isoptype6) opalu2result = ~gprdata2;
-	else                opalu2result = gprdata2;
+	case (instrbufdato0[2:0])
+	0:       opalu2result = gprdata1 << gprdata2[CLOG2ARCHBITSZ-1:0];
+	1:       opalu2result = gprdata1 >> gprdata2[CLOG2ARCHBITSZ-1:0];
+	2:       opalu2result = $signed(gprdata1) >>> gprdata2[CLOG2ARCHBITSZ-1:0];
+	3:       opalu2result = gprdata1 & gprdata2;
+	4:       opalu2result = gprdata1 | gprdata2;
+	5:       opalu2result = gprdata1 ^ gprdata2;
+	6:       opalu2result = ~gprdata2;
+	default: opalu2result = gprdata2;
+	endcase
 end
 `ifdef PUSC2
 always @* begin
-	if      (sc2isoptype0) sc2opalu2result = sc2gprdata1 << sc2gprdata2[CLOG2ARCHBITSZ-1:0];
-	else if (sc2isoptype1) sc2opalu2result = sc2gprdata1 >> sc2gprdata2[CLOG2ARCHBITSZ-1:0];
-	else if (sc2isoptype2) sc2opalu2result = $signed(sc2gprdata1) >>> sc2gprdata2[CLOG2ARCHBITSZ-1:0];
-	else if (sc2isoptype3) sc2opalu2result = sc2gprdata1 & sc2gprdata2;
-	else if (sc2isoptype4) sc2opalu2result = sc2gprdata1 | sc2gprdata2;
-	else if (sc2isoptype5) sc2opalu2result = sc2gprdata1 ^ sc2gprdata2;
-	else if (sc2isoptype6) sc2opalu2result = ~sc2gprdata2;
-	else                   sc2opalu2result = sc2gprdata2;
+	case (sc2instrbufdato0[2:0])
+	0:       sc2opalu2result = sc2gprdata1 << sc2gprdata2[CLOG2ARCHBITSZ-1:0];
+	1:       sc2opalu2result = sc2gprdata1 >> sc2gprdata2[CLOG2ARCHBITSZ-1:0];
+	2:       sc2opalu2result = $signed(sc2gprdata1) >>> sc2gprdata2[CLOG2ARCHBITSZ-1:0];
+	3:       sc2opalu2result = sc2gprdata1 & sc2gprdata2;
+	4:       sc2opalu2result = sc2gprdata1 | sc2gprdata2;
+	5:       sc2opalu2result = sc2gprdata1 ^ sc2gprdata2;
+	6:       sc2opalu2result = ~sc2gprdata2;
+	default: sc2opalu2result = sc2gprdata2;
+	endcase
 end
 `endif
 
 `ifdef PUDSPMUL
 always @* begin
 	// Implement mulu, mulhu, mul, mulh.
-	if      (isoptype0) opdspmulresult = opdspmulresult_unsigned[ARCHBITSZ-1:0];
-	else if (isoptype1) opdspmulresult = opdspmulresult_unsigned[(ARCHBITSZ*2)-1:ARCHBITSZ];
-	else if (isoptype2) opdspmulresult = opdspmulresult_signed[ARCHBITSZ-1:0];
-	else                opdspmulresult = opdspmulresult_signed[(ARCHBITSZ*2)-1:ARCHBITSZ];
+	case (instrbufdato0[2:0])
+	0:       opdspmulresult = opdspmulresult_unsigned[ARCHBITSZ-1:0];
+	1:       opdspmulresult = opdspmulresult_unsigned[(ARCHBITSZ*2)-1:ARCHBITSZ];
+	2:       opdspmulresult = opdspmulresult_signed[ARCHBITSZ-1:0];
+	default: opdspmulresult = opdspmulresult_signed[(ARCHBITSZ*2)-1:ARCHBITSZ];
+	endcase
 end
 `ifdef PUSC2
 always @* begin
-	if      (sc2isoptype0) sc2opdspmulresult = sc2opdspmulresult_unsigned[ARCHBITSZ-1:0];
-	else if (sc2isoptype1) sc2opdspmulresult = sc2opdspmulresult_unsigned[(ARCHBITSZ*2)-1:ARCHBITSZ];
-	else if (sc2isoptype2) sc2opdspmulresult = sc2opdspmulresult_signed[ARCHBITSZ-1:0];
-	else                   sc2opdspmulresult = sc2opdspmulresult_signed[(ARCHBITSZ*2)-1:ARCHBITSZ];
+	case (sc2instrbufdato0[2:0])
+	0:       sc2opdspmulresult = sc2opdspmulresult_unsigned[ARCHBITSZ-1:0];
+	1:       sc2opdspmulresult = sc2opdspmulresult_unsigned[(ARCHBITSZ*2)-1:ARCHBITSZ];
+	2:       sc2opdspmulresult = sc2opdspmulresult_signed[ARCHBITSZ-1:0];
+	default: sc2opdspmulresult = sc2opdspmulresult_signed[(ARCHBITSZ*2)-1:ARCHBITSZ];
+	endcase
 end
 `endif
 `endif
--- pu/opgetsysreg.pu.v
+++ pu/opgetsysreg.pu.v
@@ -4,27 +4,30 @@
 always @* begin
 	// Implement getsysopcode, getuip, getfaultaddr, getfaultreason,
 	// getclkcyclecnt, getclkcyclecnth, gettlbsize, geticachesize.
-	if      (isoptype0) opgetsysregresult = {{(ARCHBITSZ-16){1'b0}}, sysopcode};
-	else if (isoptype1) opgetsysregresult = {uip, 1'b0};
-	else if (isoptype2) opgetsysregresult = faultaddr;
-	else if (isoptype3) opgetsysregresult = {{(ARCHBITSZ-3){1'b0}}, faultreason};
-	else if (isoptype4) opgetsysregresult = clkcyclecnt[ARCHBITSZ -1 : 0];
-	else if (isoptype5) opgetsysregresult = clkcyclecnt[(ARCHBITSZ*2) -1 : ARCHBITSZ];
-	else if (isoptype6) opgetsysregresult = TLBSETCOUNT;
-	else                opgetsysregresult = (ICACHESETCOUNT << CLOG2XARCHBITSZBY8DIFF);
+	case (instrbufdato0[2:0])
+	0:       opgetsysregresult = {{(ARCHBITSZ-16){1'b0}}, sysopcode};
+	1:       opgetsysregresult = {uip, 1'b0};
+	2:       opgetsysregresult = faultaddr;
+	3:       opgetsysregresult = {{(ARCHBITSZ-3){1'b0}}, faultreason};
+	4:       opgetsysregresult = clkcyclecnt[ARCHBITSZ -1 : 0];
+	5:       opgetsysregresult = clkcyclecnt[(ARCHBITSZ*2) -1 : ARCHBITSZ];
+	6:       opgetsysregresult = TLBSETCOUNT;
+	default: opgetsysregresult = (ICACHESETCOUNT << CLOG2XARCHBITSZBY8DIFF);
+	endcase
 end
 
 always @* begin
 	// Implement getcoreid, getclkfreq, getdcachesize, gettlb, getcap, getver.
-	if      (isoptype0) opgetsysreg1result = id_i;
-	else if (isoptype1) opgetsysreg1result = CLKFREQ;
+	case (instrbufdato0[2:0])
+	0:       opgetsysreg1result = id_i;
+	1:       opgetsysreg1result = CLKFREQ;
 	`ifdef PUDCACHE
-	else if (isoptype2) opgetsysreg1result = (DCACHESETCOUNT << CLOG2XARCHBITSZBY8DIFF);
+	2:       opgetsysreg1result = (DCACHESETCOUNT << CLOG2XARCHBITSZBY8DIFF);
 	`endif
 	`ifdef PUMMU
-	else if (isoptype3) opgetsysreg1result = opgettlbresult;
+	3:       opgetsysreg1result = opgettlbresult;
 	`endif
-	else if (isoptype4) opgetsysreg1result = // 16bits value returned to take PU16 into account.
+	4:       opgetsysreg1result = // 16bits value returned to take PU16 into account.
 		{{14{1'b0}}
 		`ifdef PUMMU
 		`ifdef PUHPTW
@@ -37,6 +40,7 @@ always @* begin
 		, 2'b00
 		`endif
 		};
-	else if (isoptype5) opgetsysreg1result = VERSION; // 16bits value returned to take PU16 into account.
-	else                opgetsysreg1result = 0;
+	5:       opgetsysreg1result = VERSION; // 16bits value returned to take PU16 into account.
+	default: opgetsysreg1result = 0;
+	endcase
 end
