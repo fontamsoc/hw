b87bdd91bb5ed11bea44b25540d4994dfbd1f011
William Tambe <tambewilliam@gmail.com>
2024-02-28 14:35:22 -0600

    lib/wb_mux.v: remove extra slvidx_bsy cycle

--- lib/wb_mux.v
+++ lib/wb_mux.v
@@ -88,98 +88,71 @@ reg addrspace_rdy;
 
 reg [CLOG2SLAVECOUNT -1 : 0] slvidx;
 reg slvidx_rdy;
-reg slvidx_bsy;
 
-reg [ARCHBITSZ -1 : 0] _s_wb_mapsz_i_slvidx;
-reg [ARCHBITSZ -1 : 0] addrspace_slvidx;
 reg [ARCHBITSZ -1 : 0] addrspace_slvidx_lo; // Also used to initialize addrspace.
+reg [ARCHBITSZ -1 : 0] addrspace_slvidx_hi;
 
 wire slvidx_not_max = (slvidx < (SLAVECOUNT-1));
 
 // Determine whether slvidx needs to be recomputed.
-wire slvidx_invalid = (slvidx_bsy || (m_wb_cyc_i && m_wb_stb_i &&
+wire slvidx_invalid = (!addrspace_rdy || (
+	m_wb_cyc_i && m_wb_stb_i &&
 	!(_m_wb_addr_i >= addrspace_slvidx_lo &&
-	  _m_wb_addr_i <= addrspace_slvidx)));
+	  _m_wb_addr_i <= addrspace_slvidx_hi)));
 
-wire [ARCHBITSZ -1 : 0] _s_wb_mapsz_i_slvidx_plus_addrspace_slvidx_lo =
-	(_s_wb_mapsz_i_slvidx + addrspace_slvidx_lo);
+wire [ARCHBITSZ -1 : 0] addrspace_slvidx_nxt = (addrspace_slvidx_lo + _s_wb_mapsz_i[slvidx]);
 
 always @ (posedge clk_i) begin
-
-	_s_wb_mapsz_i_slvidx <= _s_wb_mapsz_i[slvidx];
-
-	addrspace_slvidx <= addrspace[slvidx];
 
 	// Logic which on reset computes addrspace
 	// using the size of each slave device mapping;
 	// and after reset computes slvidx using addrspace.
 	if (rst_i) begin
 
-		addrspace_rdy <= 1'b0;
-
-		slvidx <= 0;
-
-		slvidx_rdy <= 1'b0;
-
 		addrspace_slvidx_lo <= FIRSTSLAVEADDR;
-
-		slvidx_bsy <= 1'b1;
-
-	end else if (slvidx_bsy) begin
-
-		slvidx_bsy <= 1'b0;
+		slvidx <= 0;
+		slvidx_rdy <= 1'b0;
+		addrspace_rdy <= 1'b0;
 
 	end else if (!addrspace_rdy) begin
 
 		if (slvidx_not_max) begin
-
-			addrspace_slvidx_lo <= _s_wb_mapsz_i_slvidx_plus_addrspace_slvidx_lo;
-
+			addrspace_slvidx_lo <= addrspace_slvidx_nxt;
 			slvidx <= slvidx + 1'b1;
-
-			slvidx_bsy <= 1'b1;
-
 		end else begin
-
 			addrspace_slvidx_lo <= FIRSTSLAVEADDR;
-
+			addrspace_slvidx_hi <= addrspace[0];
 			slvidx <= 0;
-
 			addrspace_rdy <= 1'b1;
-
-			slvidx_bsy <= 1'b1;
-
 			// Next state will be for:
 			// slvidx_rdy == 0 with slvidx == 0;
 			// That state will compute slvidx as though it was due to slvidx_invalid;
 			// this state set addrspace_slvidx_lo for the next state.
 		end
 
-		addrspace[slvidx] <= _s_wb_mapsz_i_slvidx_plus_addrspace_slvidx_lo - 1'b1;
+		addrspace[slvidx] <= addrspace_slvidx_nxt - 1'b1;
 
 	end else if (!slvidx_rdy) begin
 
 		if (!slvidx_invalid)
 			slvidx_rdy <= 1'b1;
 		else if (slvidx_not_max) begin
-			addrspace_slvidx_lo <= addrspace_slvidx + 1'b1;
+			addrspace_slvidx_lo <= addrspace[slvidx] + 1'b1;
+			addrspace_slvidx_hi <= addrspace[slvidx + 1'b1];
 			slvidx <= slvidx + 1'b1;
-			slvidx_bsy <= 1'b1;
 		end else begin
 			addrspace_slvidx_lo <= _m_wb_addr_i;
+			addrspace_slvidx_hi <= addrspace[DEFAULTSLAVEINDEX];
 			slvidx <= DEFAULTSLAVEINDEX;
-			if (slvidx == DEFAULTSLAVEINDEX) begin
-				// Set after slvidx has been used to compute addrspace_slvidx.
-				slvidx_rdy <= 1'b1;
-			end
-			slvidx_bsy <= 1'b1;
+			slvidx_rdy <= 1'b1;
 		end
 
 	end else if (slvidx_invalid) begin
-		slvidx_rdy <= 1'b0;
-		slvidx <= 0;
+
 		addrspace_slvidx_lo <= FIRSTSLAVEADDR;
-		slvidx_bsy <= 1'b1;
+		addrspace_slvidx_hi <= addrspace[0];
+		slvidx <= 0;
+		slvidx_rdy <= 1'b0;
 	end
 end
 
