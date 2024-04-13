52a740672730430dea4becc7a2ededea8a458358
William Tambe <tambewilliam@gmail.com>
2024-04-12 21:17:21 -0500

    WIP: wishbone: pipelined wb_dnsizr wb_upsizr

--- lib/wb_upsizr.v
+++ lib/wb_upsizr.v
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // (c) William Fonkou Tambe
 
-`ifndef WB_DNSIZR_V
-`define WB_DNSIZR_V
+`ifndef WB_UPSIZR_V
+`define WB_UPSIZR_V
 
-// (MARCHBITSZ >= SARCHBITSZ) must be true.
+// (MARCHBITSZ <= SARCHBITSZ) must be true.
 
-`include "lib/addr.v"
 `include "lib/fifo.v"
 `include "lib/fifo_fwft.v"
 
-module wb_dnsizr (
+module wb_upsizr (
 
 	 rst_i
 
@@ -45,8 +44,6 @@ parameter SARCHBITSZ = 0;
 parameter MAXPENDINGACK = 8; // It must be at least 2 and a power of 2.
 
 parameter USEFWFTFIFO = 0;
-
-localparam CLOG2SARCHBITSZ = clog2(SARCHBITSZ);
 
 localparam CLOG2MARCHBITSZBY8 = clog2(MARCHBITSZ/8);
 localparam CLOG2SARCHBITSZBY8 = clog2(SARCHBITSZ/8);
@@ -84,62 +81,55 @@ assign s_wb_we_o = m_wb_we_i;
 wire m_wb_bsy_o_;
 assign m_wb_bsy_o = (m_wb_bsy_o_ || s_wb_bsy_i);
 
-generate if (MARCHBITSZ > SARCHBITSZ) begin :gen_dnsizr
+generate if (MARCHBITSZ < SARCHBITSZ) begin :gen_upsizr
 
-	wire [MARCHBITSZ -1 : 0] _m_wb_addr_i;
+	assign s_wb_addr_o = {
+		{(SADDRBITSZ-(MADDRBITSZ-(CLOG2SARCHBITSZBY8-CLOG2MARCHBITSZBY8))){1'b0}},
+		m_wb_addr_i[MADDRBITSZ -1 : (CLOG2SARCHBITSZBY8-CLOG2MARCHBITSZBY8)]};
 
-	addr #(
-		.ARCHBITSZ (MARCHBITSZ)
-	) addr (
-		 .addr_i (m_wb_addr_i)
-		,.sel_i  (m_wb_sel_i)
-		,.addr_o (_m_wb_addr_i)
-	);
+	wire [MADDRBITSZ -1 : 0] _m_wb_addr_i;
 
-	assign s_wb_addr_o = _m_wb_addr_i[SARCHBITSZ -1 : CLOG2SARCHBITSZBY8];
-
-	wire [SADDRBITSZ -1 : 0] _s_wb_addr_o;
-
-	if (USEFWFTFIFO) begin :gen_dnsizr_fifo_fwft
+	if (USEFWFTFIFO) begin :gen_upsizr_fifo_fwft
 	fifo_fwft #(
-		 .WIDTH (SADDRBITSZ)
+		 .WIDTH (MADDRBITSZ)
 		,.DEPTH (MAXPENDINGACK)
 	) fifo_fwft (
 		 .rst_i      (rst_i)
 		,.clk_push_i (clk_i)
 		,.push_i     (m_wb_cyc_i && m_wb_stb_i && !s_wb_bsy_i)
-		,.data_i     (s_wb_addr_o)
+		,.data_i     (m_wb_addr_i)
 		,.full_o     (m_wb_bsy_o_)
 		,.clk_pop_i  (clk_i)
 		,.pop_i      (s_wb_ack_i)
-		,.data_o     (_s_wb_addr_o)
+		,.data_o     (_m_wb_addr_i)
 	);
-	end else begin :gen_dnsizr_fifo
+	end else begin :gen_upsizr_fifo
 	fifo #(
-		 .WIDTH (SADDRBITSZ)
+		 .WIDTH (MADDRBITSZ)
 		,.DEPTH (MAXPENDINGACK)
 	) fifo (
 		 .rst_i       (rst_i)
 		,.clk_write_i (clk_i)
 		,.write_i     (m_wb_cyc_i && m_wb_stb_i && !s_wb_bsy_i)
-		,.data_i      (s_wb_addr_o)
+		,.data_i      (m_wb_addr_i)
 		,.full_o      (m_wb_bsy_o_)
 		,.clk_read_i  (clk_i)
 		,.read_i      (s_wb_ack_i)
-		,.data_o      (_s_wb_addr_o)
+		,.data_o      (_m_wb_addr_i)
 	);
 	end
 
 	reg [SARCHBITSZ -1 : 0] _s_wb_dat_i;
 
-	assign m_wb_dat_o = ({{(MARCHBITSZ-SARCHBITSZ){1'b0}}, _s_wb_dat_i} <<
-		{_s_wb_addr_o[(CLOG2MARCHBITSZBY8-CLOG2SARCHBITSZBY8) -1 : 0], {CLOG2SARCHBITSZ{1'b0}}});
+	assign m_wb_dat_o = {_s_wb_dat_i >>
+		(_m_wb_addr_i[(CLOG2SARCHBITSZBY8-CLOG2MARCHBITSZBY8) -1 : 0]*MARCHBITSZ)};
 
-	assign s_wb_dat_o = {m_wb_dat_i >>
-		{s_wb_addr_o[(CLOG2MARCHBITSZBY8-CLOG2SARCHBITSZBY8) -1 : 0], {CLOG2SARCHBITSZ{1'b0}}}};
+	assign s_wb_dat_o = (
+		{{(SARCHBITSZ-MARCHBITSZ){1'b0}}, m_wb_dat_i} <<
+		(m_wb_addr_i[(CLOG2SARCHBITSZBY8-CLOG2MARCHBITSZBY8) -1 : 0]*MARCHBITSZ));
 
-	assign s_wb_sel_o = {m_wb_sel_i >>
-		{s_wb_addr_o[(CLOG2MARCHBITSZBY8-CLOG2SARCHBITSZBY8) -1 : 0], {CLOG2SARCHBITSZBY8{1'b0}}}};
+	assign s_wb_sel_o = ({{((SARCHBITSZ-MARCHBITSZ)/8){1'b0}}, m_wb_sel_i} <<
+		(m_wb_addr_i[(CLOG2SARCHBITSZBY8-CLOG2MARCHBITSZBY8) -1 : 0]*(MARCHBITSZ/8)));
 
 	if (USEFWFTFIFO) begin
 	always @* begin
@@ -169,4 +159,4 @@ end endgenerate
 
 endmodule
 
-`endif /* WB_DNSIZR_V */
+`endif /* WB_UPSIZR_V */
