08be51ee372f1840a6fef2be2de5bd6ec58b4408
William Tambe <tambewilliam@gmail.com>
2024-05-04 09:15:45 -0500

    lib/wb_mux.v: fix default slave index

--- lib/wb_mux.v
+++ lib/wb_mux.v
@@ -107,6 +107,7 @@ reg addrspace_rdy;
 
 reg [CLOG2SLAVECOUNT -1 : 0] slvidx;
 reg slvidx_rdy;
+reg slvidx_dflt;
 
 reg [ARCHBITSZ -1 : 0] addrspace_slvidx_lo; // Also used to initialize addrspace.
 reg [ARCHBITSZ -1 : 0] addrspace_slvidx_hi;
@@ -115,7 +116,7 @@ wire slvidx_not_max = (slvidx < (SLAVECOUNT-1));
 
 // Determine whether slvidx needs to be recomputed.
 wire slvidx_invalid = (!addrspace_rdy || (
-	_m_wb_stb_i &&
+	!slvidx_dflt && _m_wb_stb_i &&
 	!(_m_wb_addr_i >= addrspace_slvidx_lo &&
 	  _m_wb_addr_i <= addrspace_slvidx_hi)));
 
@@ -130,8 +131,9 @@ always @ (posedge clk_i) begin
 
 		addrspace_slvidx_lo <= FIRSTSLAVEADDR;
 		slvidx <= 0;
-		slvidx_rdy <= 1'b0;
-		addrspace_rdy <= 1'b0;
+		slvidx_rdy <= 0;
+		slvidx_dflt <= 0;
+		addrspace_rdy <= 0;
 
 	end else if (!addrspace_rdy) begin
 
@@ -142,7 +144,7 @@ always @ (posedge clk_i) begin
 			addrspace_slvidx_lo <= FIRSTSLAVEADDR;
 			addrspace_slvidx_hi <= addrspace[0];
 			slvidx <= 0;
-			addrspace_rdy <= 1'b1;
+			addrspace_rdy <= 1;
 			// Next state will be for:
 			// slvidx_rdy == 0 with slvidx == 0;
 			// That state will compute slvidx as though it was due to slvidx_invalid;
@@ -154,7 +156,7 @@ always @ (posedge clk_i) begin
 	end else if (!slvidx_rdy) begin
 
 		if (!slvidx_invalid)
-			slvidx_rdy <= 1'b1;
+			slvidx_rdy <= 1;
 		else if (slvidx_not_max) begin
 			addrspace_slvidx_lo <= addrspace[slvidx] + 1'b1;
 			addrspace_slvidx_hi <= addrspace[slvidx + 1'b1];
@@ -163,7 +165,8 @@ always @ (posedge clk_i) begin
 			addrspace_slvidx_lo <= _m_wb_addr_i;
 			addrspace_slvidx_hi <= addrspace[DEFAULTSLAVEINDEX];
 			slvidx <= DEFAULTSLAVEINDEX;
-			slvidx_rdy <= 1'b1;
+			slvidx_rdy <= 1;
+			slvidx_dflt <= 1;
 		end
 
 	end else if (slvidx_invalid && !ack_pending) begin
@@ -171,8 +174,10 @@ always @ (posedge clk_i) begin
 		addrspace_slvidx_lo <= FIRSTSLAVEADDR;
 		addrspace_slvidx_hi <= addrspace[0];
 		slvidx <= 0;
-		slvidx_rdy <= 1'b0;
-	end
+		slvidx_rdy <= 0;
+
+	end else if (slvidx_dflt)
+		slvidx_dflt <= m_wb_bsy_o;
 end
 
 assign m_wb_bsy_o = ((slvidx_invalid ? 1'b1 : s_wb_bsy_i[slvidx]) || (ack_pending == MAXPENDINGACK));
