6a026765ae59f990897c7346a0f254f097fd5127
William Tambe <tambewilliam@gmail.com>
2025-04-28 23:23:16 -0500

    riscv: do not display perf info by default

--- rvxx/sys.pu.v
+++ rvxx/sys.pu.v
@@ -165,7 +165,7 @@ always @ (posedge clk_i) begin
 	csrClkFreq <= CLKFREQ;
 end
 
-`ifdef SIMULATION
+`ifdef _SIMULATION
 `ifdef PUPREDICTBRANCH
 always @ (posedge clk_i) begin
 	if (rst_i) begin
@@ -213,6 +213,7 @@ always @ (posedge clk_i) begin
 end
 always @ (posedge clk_i) begin
 	if (endSimRq && !wb_pending_acks) begin
+		`ifdef _SIMULATION
 		`ifdef PUPREDICTBRANCH
 		$write ("BranchPredictHit: %1.2f%%\n",
 			($bitstoreal(csrBranchPredictHit * 100) / $bitstoreal(csrBranchPredictHit + csrBranchPredictMiss)));
@@ -222,6 +223,7 @@ always @ (posedge clk_i) begin
 		`endif
 		$write ("CPI: %1.2f\n", ($bitstoreal(csrCycle) / $bitstoreal(csrInstret)));
 		$fflush();
+		`endif
 		$finish;
 	end
 end
