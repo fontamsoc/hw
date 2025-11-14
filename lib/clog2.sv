25ea3d1fdd25317311a656da10ddf573672cb50f
William Tambe <tambewilliam@gmail.com>
2025-11-14 14:33:49 -0600

    lib/clog2: use systemVerilog compound assignment operators

--- lib/clog2.sv
+++ lib/clog2.sv
@@ -4,12 +4,10 @@
 // Function which compute the ceiling of log2().
 // When the argument is 0 or 1, the return value is 1.
 function automatic integer clog2;
-	input integer val;
-	begin
-		if (val > 1) begin
-			val = val - 1;
-			for (clog2 = 0; val > 0; clog2 = clog2 + 1)
-				val = val >> 1;
-		end else clog2 = 1;
-	end
+	input int unsigned val;
+	if (val > 1) begin
+		val -= 1;
+		for (clog2 = 0; val > 0; ++clog2)
+			val >>= 1;
+	end else clog2 = 1;
 endfunction
