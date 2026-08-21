70f1b97e19cf75e5a77730c2fb01fb3d7fcbfad9
William Tambe <tambewilliam@gmail.com>
2026-08-21 09:28:25 -0500

    rv32 tops: remove comment above default_nettype none

--- rv32-orangecrab0285/orangecrab0285.sv
+++ rv32-orangecrab0285/orangecrab0285.sv
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // (c) William Fonkou Tambe
 
-// This directive prevent verilog from
-// automatically declaring undefined net.
-// The correct and sane behavior is to throw
-// an error when an undefined net is used.
 `default_nettype none
 
 `include "lib/ecppll_48_to_24_48_96.sv"
